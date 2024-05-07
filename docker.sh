763c56882a5c9f8b2fb85c464ed67e6358686dae
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-05-07 08:10:42 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install docker.io -y
+
+sudo usermod -aG docker $USER  # Replace with your system's username, e.g., 'ubuntu'
+
+newgrp docker
+
+sudo chmod 777 /var/run/docker.sock
